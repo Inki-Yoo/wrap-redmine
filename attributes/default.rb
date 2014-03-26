d29c92dd3e568762f5863e8511e5b291342431bd
trilitheus <trilitheus@gmail.com>
2014-03-26 23:31:50 +0000

    Updated vault retrieval

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,6 @@
 default['redmine']['script_dir'] = node['redmine']['home'] + '/scripts'
+
+override['gitlab']['nginx_server_names'] = ['gitlab.*', 'trigitlab.*']
 override['gitlab']['install_ruby'] = 'package'
 override['gitlab']['cookbook_dependencies'] = %w[
   build-essential git openssh readline xml zlib sudo ruby_build
--- recipes/default.rb
+++ recipes/default.rb
@@ -4,6 +4,7 @@ require 'chef-vault'
 
 redmine_secrets = ChefVault::Item.load('secrets', 'redmine')
 node.override['gitlab']['database']['password'] = redmine_secrets['password']
+node.override['redmine']['dbpass'] = redmine_secrets['password']
 
 include_recipe 'redmine'
 
@@ -11,9 +12,10 @@ include_recipe 'wrapper-redmine::redmine_plugins'
 
 include_recipe 'wrapper-redmine::sync_repos'
 
-ruby_block 'wipe gitlab password' do
+ruby_block 'wipe gitlab and redmine passwords' do
   block do
     node.force_override['gitlab']['database']['password'] = ''
+    node.force_override['redmine']['dbpass'] = ''
   end
   action :create
 end
