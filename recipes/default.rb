939a3394d65f806a39445b4cf1696fe65e4967ec
trilitheus <trilitheus@gmail.com>
2014-03-26 23:50:22 +0000

    v0.1.4 Added gitlab by atomic penguin

--- recipes/default.rb
+++ recipes/default.rb
@@ -12,10 +12,12 @@ include_recipe 'wrapper-redmine::redmine_plugins'
 
 include_recipe 'wrapper-redmine::sync_repos'
 
+include_recipe 'gitlab'
+
 ruby_block 'wipe gitlab and redmine passwords' do
   block do
-    node.force_override['gitlab']['database']['password'] = ''
-    node.force_override['redmine']['dbpass'] = ''
+    node.force_override['gitlab']['database']['password'] = '<redacted>'
+    node.force_override['redmine']['dbpass'] = '<redacted>'
   end
   action :create
 end
